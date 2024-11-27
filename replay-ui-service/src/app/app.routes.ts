32cc85220447e1ed28cce4305eb2e9bc668df8fb
jack <1037289945@qq.com>
2024-11-27 17:09:02 +0800

    init

--- replay-ui-service/src/app/app.routes.ts
+++ replay-ui-service/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import {Routes} from '@angular/router';
+import {SearchPageComponent} from './search-page/search-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '/search', component: SearchPageComponent
+  }
+];
